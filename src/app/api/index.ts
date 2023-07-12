37dbacfb68810f3781dab260bd192221a95c1062
Binu Senevirathne <bikumar@noon.com>
2023-07-13 03:54:25 +0530

    Update index.ts

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -7,7 +7,6 @@ import campaignData from './campaignData/campaignData';
 import dashboard from './dashboard/dashboard';
 import { TDashboardSVCEndpoints } from './dashboard/dashboard.types';
 
-
 //  API base URL - Change this correctly
 // const BASE_URL = 'http://192.168.8.187:3001/_svc/block-vote/api/v1';
 export const BASE_URL = 'http://localhost:3001/_svc/block-vote/api/v1';
